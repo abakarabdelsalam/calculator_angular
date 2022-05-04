63191b6f46240116fbb18fffae223fcc224380f0
abdelsalam <abdelsalam.abakar@yahoo.fr>
2022-05-04 21:17:02 +0200

    logical methods

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,8 +7,75 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
+    currentNumber = '0';
+    firstOperand:any = null;
+    operator:any = null;
+    waitForSecondNumber = false;
+
   constructor() { }
 
+//Méthode pour obtenir le numéro actuel
+  public getNumber(v: string){
+    console.log(v);
+    if(this.waitForSecondNumber)
+    {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    }else{
+      this.currentNumber === '0'? this.currentNumber = v: this.currentNumber += v;
+
+    }
+  }
+
+  //Méthode qui ajoute la virgule au nombre décimale actuel 
+  getDecimal(){
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.'; 
+    }
+  }
+
+  //Méthode qui effectue le calcul en fonction du type d'opérateur
+  private doCalculation(op:any , secondOp:any): any{
+    switch (op){
+      case '+':
+      return this.firstOperand += secondOp; 
+      case '-': 
+      return this.firstOperand -= secondOp; 
+      case '*': 
+      return this.firstOperand *= secondOp; 
+      case '/': 
+      return this.firstOperand /= secondOp; 
+      case '=':
+      return secondOp;
+    }
+  }
+
+  //Métode pour obtenir l'opération effectuée
+  public getOperation(op: string){
+    console.log(op);
+
+    if(this.firstOperand === null){
+      this.firstOperand = Number(this.currentNumber);
+
+    }else if(this.operator){
+      const result = this.doCalculation(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+
+  //Méthode pour effacer la zone de résultat et réinitialiser les calculs
+  public clear(){
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
   ngOnInit(): void {
   }
 
